0b42fcd52fdb990864a0823d176b8686ce63a2be
Aseem PM <a.palumannil@edexa.team>
2024-06-04 16:43:08 +0530

    updated the variable name

--- ignition/modules/EdexaTokenPool.ts
+++ ignition/modules/EdexaTokenPool.ts
@@ -1,10 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const { GATEWAY_ADDRESS, POOL_ADDRESS } = process.env;
+const { GATEWAY_ADDRESS, POOL_TOKEN_ADDRESS } = process.env;
 
 const EdexaTokenPoolModule = buildModule("EdexaTokenPoolModule", (m) => {
   const gatewayAddress = m.getParameter("_wmbGateway", GATEWAY_ADDRESS);
-  const poolAddress = m.getParameter("_poolToken", POOL_ADDRESS);
+  const poolAddress = m.getParameter("_poolToken", POOL_TOKEN_ADDRESS);
 
   const edexaTokenPool = m.contract("EdexaTokenPool", [
     gatewayAddress,
