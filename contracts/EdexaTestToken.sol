70452f5deef847c13d212927bda31561eb48dbcb
Aseem PM <a.palumannil@edexa.team>
2024-06-04 12:00:44 +0530

    added edexa test token

---
+++ contracts/EdexaTestToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract EdexaTestToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    uint256 public constant MAX_SUPPLY = 10_000_000_000 * 10 ** 18; // Maximum supply: 10 billion tokens
+
+    constructor() ERC20("EdeXaTestToken", "ETT") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(
+            totalSupply() + amount <= MAX_SUPPLY,
+            "Exceeds maximum supply of 10 billion"
+        );
+        _mint(to, amount);
+    }
+
+    function _udpate(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual whenNotPaused {
+        super._update(from, to, amount);
+    }
+}
