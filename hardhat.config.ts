a51cdad22b45d8236b2c56d7486b81fee89bcb9a
Aseem PM <a.palumannil@edexa.team>
2024-06-04 12:36:27 +0530

    configurated the network file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
+const TEST_PRIVATE_KEY = vars.get("TEST_PRIVATE_KEY");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    edexaTestnet: {
+      accounts: [TEST_PRIVATE_KEY],
+      url: "https://testnet.edexa.network/rpc",
+    },
+    amoy: {
+      accounts: [TEST_PRIVATE_KEY],
+      url: "https://rpc-amoy.polygon.technology",
+    },
+  },
 };
 
 export default config;
