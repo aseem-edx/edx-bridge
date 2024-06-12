26438d5fe52039b1a410fb7501975842dc13ab02
Aseem PM <a.palumannil@edexa.team>
2024-06-12 16:04:36 +0530

    changed token address passing to pool

--- ignition/modules/EdexaCCPool.ts
+++ ignition/modules/EdexaCCPool.ts
@@ -1,12 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const { GATEWAY_ADDRESS, TOKEN_ADDRESS } = process.env;
+const { GATEWAY_ADDRESS } = process.env;
 
 const EdexaCCPoolModule = buildModule("EdexaCCPoolModule", (m) => {
   const gatewayAddress = m.getParameter("_wmbGateway", GATEWAY_ADDRESS);
-  const tokenAddress = m.getParameter("_token", TOKEN_ADDRESS);
 
-  const edexaCCPool = m.contract("EdexaCCPool", [gatewayAddress, tokenAddress]);
+  const edexaCCPool = m.contract("EdexaCCPool", [gatewayAddress]);
 
   return { edexaCCPool };
 });
