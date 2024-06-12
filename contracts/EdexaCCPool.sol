3cd9253e454770194b13478b6b3641d9998c5be3
Aseem PM <a.palumannil@edexa.team>
2024-06-12 16:00:57 +0530

    changed contract version

--- contracts/EdexaCCPool.sol
+++ contracts/EdexaCCPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
 import "@wandevs/message/contracts/app/WmbApp.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
