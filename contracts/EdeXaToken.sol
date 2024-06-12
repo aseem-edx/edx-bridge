a6a557fe5247222a9b7ca33072181b49e823e60c
Aseem PM <a.palumannil@edexa.team>
2024-06-12 16:03:26 +0530

    used edexa version of token

--- contracts/EdeXaToken.sol
+++ contracts/EdeXaToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EdexaToken is ERC20, ERC20Burnable, Pausable, Ownable {
     uint256 public constant MAX_SUPPLY = 10_000_000_000 * 10 ** 18; // Maximum supply: 10 billion tokens
@@ -12,7 +12,15 @@ contract EdexaToken is ERC20, ERC20Burnable, Pausable, Ownable {
     constructor(
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol) Ownable(msg.sender) {}
+    ) ERC20(_name, _symbol) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         require(
@@ -22,11 +30,11 @@ contract EdexaToken is ERC20, ERC20Burnable, Pausable, Ownable {
         _mint(to, amount);
     }
 
-    function _udpate(
+    function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal virtual whenNotPaused {
-        super._update(from, to, amount);
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
