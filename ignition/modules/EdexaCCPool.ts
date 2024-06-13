c9ff529817f5e07cc5157906955684d5dfb7a0df
Aseem PM <a.palumannil@edexa.team>
2024-06-13 15:21:52 +0530

    updated ignition script o have constructor

--- ignition/modules/EdexaCCPool.ts
+++ ignition/modules/EdexaCCPool.ts
@@ -1,7 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
+const { GATEWAY } = process.env;
+
 const EdexaCCPoolModule = buildModule("EdexaCCPoolModule", (m) => {
-  const edexaCCPool = m.contract("EdexaCCPool");
+  const gateway = m.getParameter("_wmbGateway", GATEWAY!);
+
+  const edexaCCPool = m.contract("EdexaCCPool", [gateway]);
 
   return { edexaCCPool };
 });
