4e921697b58e8e8c62f55bfa2a0e2ed9c2e87b65
Aseem PM <a.palumannil@edexa.team>
2024-06-13 13:22:26 +0530

    added cross token script

---
+++ scripts/02_cross_token.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+
+const { SOURCE_POOL, SOURCE_TOKEN, DESTINATION_CHAIN_ID } = process.env;
+
+const ONE_WEI = 1;
+
+async function crossToken() {
+  const [signer] = await ethers.getSigners();
+
+  const sourcePool = await ethers.getContractAt("EdexaCCPool", SOURCE_POOL!);
+  const sourceToken = await ethers.getContractAt("EdexaToken", SOURCE_TOKEN!);
+
+  const approveAmount = await sourceToken.approve(signer, ONE_WEI!);
+  await approveAmount.wait();
+  console.log("Txn - Approve Amount", approveAmount.hash);
+
+  const crossToken = await sourcePool.crossTo(
+    DESTINATION_CHAIN_ID!,
+    signer,
+    ONE_WEI
+  );
+  await crossToken.wait();
+  console.log("Txn - Cross Token", crossToken.hash);
+}
+
+crossToken()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
